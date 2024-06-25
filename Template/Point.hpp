99b27b41fb8cdd97477ec4085afa3869a85de516
chj_Xu <chengjun_xu@163.com>
2024-06-25 12:10:07 +0000

    更新Point3D

--- Template/Point.hpp
+++ Template/Point.hpp
@@ -12,7 +12,14 @@ struct PixelPoint
     float v;
 };
 
+struct WorldPoint
+{
+    float x;
+    float y;
+    float z;
+};
 
+/****************************************** Point2D **************************************************/
 // Point2 dim
 template<typename _Tp>
 class _Point2
@@ -94,7 +101,100 @@ bool _Point2<_Tp>::isZero() const
 {
     return std::fabs(_x - _Tp(0)) < 1e-4 &&  std::fabs(_y - _Tp(0)) < 1e-4;
 }
-
 using Point2i = _Point2<int>;
 using Point2f = _Point2<float>;
 using Point2d = _Point2<double>;
+/*****************************************************************************************************/
+
+/****************************************** Point3D **************************************************/
+// Point3 dim
+template<typename _Tp>
+class _Point3
+{
+public:
+    typedef _Tp value_type;
+
+    _Point3();
+    _Point3(_Tp x, _Tp y, _Tp z);
+    _Point3(const _Point3& pt);
+
+    _Point3& operator=(const _Point3& pt);
+    _Point3 operator-(const _Point3& pt);
+
+    value_type x() const;
+    value_type y() const;
+    value_type z() const;
+
+    template<typename _Tp2>
+    void copyFrom(const _Tp2& pt);
+
+    void copyFrom(const WorldPoint& pt);
+
+    bool isZero() const;
+private:
+    _Tp _x;
+    _Tp _y;
+    _Tp _z;
+};
+
+template<typename _Tp> inline
+_Point3<_Tp>::_Point3() : _x(_Tp(0)), _y(_Tp(0)), _z(_Tp(0)) {}
+
+template<typename _Tp> inline
+_Point3<_Tp>::_Point3(_Tp x, _Tp y, _Tp z) : _x(x), _y(y), _z(z) {}
+
+template<typename _Tp> inline
+_Point3<_Tp>::_Point3(const _Point3& pt) : _x(pt._x), _y(pt._y), _z(pt._z) {}
+
+// template<typename _Tp> inline
+// _Point3<_Tp>::_Point3(const _Tp& x, const _Tp& y, const _Tp& z) : _x(x), _y(y), _z(z) {}
+
+template<typename _Tp> inline
+_Point3<_Tp>& _Point3<_Tp>::operator=(const _Point3& pt)
+{
+    _x = pt._x;
+    _y = pt._y;
+    _z = pt._z;
+    return *this;
+}
+
+template<typename _Tp> inline
+_Point3<_Tp> _Point3<_Tp>::operator-(const _Point3& pt)
+{
+    return _Point3<_Tp>(_x - pt._x, _y - pt._y, _z - pt._z);
+}
+
+template<typename _Tp> inline
+_Tp _Point3<_Tp>::x() const { return this->_x; }
+
+template<typename _Tp> inline
+_Tp _Point3<_Tp>::y() const { return this->_y; }
+
+template<typename _Tp> inline
+_Tp _Point3<_Tp>::z() const { return this->_z; }
+
+template<typename _Tp>
+template<typename _Tp2>
+void _Point3<_Tp>::copyFrom(const _Tp2& pt)
+{
+    _x = pt.x();
+    _y = pt.y();
+    _z = pt.z();
+}
+
+template<typename _Tp>
+void _Point3<_Tp>::copyFrom(const WorldPoint& pt)
+{
+    _x = pt.x;
+    _y = pt.y;
+    _z = pt.z;
+}
+
+template<typename _Tp>
+bool _Point3<_Tp>::isZero() const
+{
+    return std::fabs(_x - _Tp(0)) < 1e-4 && std::fabs(_y - _Tp(0)) < 1e-4 && std::fabs(_z - _Tp(0)) < 1e-4;
+}
+using Point3i = _Point3<int>;
+using Point3f = _Point3<float>;
+using Point3d = _Point3<double>;
--- Template/test.cpp
+++ Template/test.cpp
@@ -3,25 +3,38 @@
 
 int main()
 {
-    // point
-    Point2i p1(1, 1);
-    Point2f p2(1.0, 2.0);
-    Point2f p3;
-    p3.copyFrom(p1);
-    Point2f p4(1.0, 3.0);
-    std::cout << p1.x() << " " << p1.y() << std::endl;
-    std::cout << p2.x() << " " << p2.y() << std::endl;
-    std::cout << p3.x() << " " << p3.y() << std::endl;
+    // point2 dim
+    Point2i p21(1, 1);
+    Point2f p22(1.0, 2.0);
+    Point2f p23;
+    p23.copyFrom(p21);
+    Point2f p24(1.0, 3.0);
+    std::cout << p21.x() << " " << p21.y() << std::endl;
+    std::cout << p22.x() << " " << p22.y() << std::endl;
+    std::cout << p23.x() << " " << p23.y() << std::endl;
+    Point2f p25;
+    p25 = p23 - p22;
+    std::cout << p25.x() << " " << p25.y() << std::endl;
+    std::cout << p25.isZero() << std::endl;
 
-    Point2f p5;
-    p5 = p3 - p2;
-    std::cout << p5.x() << " " << p5.y() << std::endl;
-    std::cout << p5.isZero() << std::endl;
+    // point3 dim
+    Point3i p31(1, 1, 1);
+    Point3f p32(1.0, 2.0, 3.0);
+    Point3f p33;
+    p33.copyFrom(p31);
+    Point3f p34(1.0, 3.0, 5.0);
+    std::cout << p31.x() << " " << p31.y() << " " << p31.z() << std::endl;
+    std::cout << p32.x() << " " << p32.y() << " " << p31.z() << std::endl;
+    std::cout << p33.x() << " " << p33.y() << " " << p31.z() << std::endl;
+    Point3f p35;
+    p35 = p33 - p32;
+    std::cout << p35.x() << " " << p35.y() << " " << p35.z() << std::endl;
+    std::cout << p35.isZero() << std::endl;
 
     // line
-    Line2f l(p3, p2);
-    std::cout << l.sp().x() <<" " << l.sp().y() << std::endl;
-    std::cout << l.ep().x() <<" " << l.ep().y() << std::endl;
-    std::cout << l.vd().x() <<" " << l.vd().y() << std::endl;
+    Line2f l(p23, p22);
+    std::cout << l.sp().x() << " " << l.sp().y() << std::endl;
+    std::cout << l.ep().x() << " " << l.ep().y() << std::endl;
+    std::cout << l.vd().x() << " " << l.vd().y() << std::endl;
     return 0;
 }
